38b886ee0898ad78613493f490b1707bb995ab60
landonkimber <landonkimber33@gmail.com>
2024-09-12 21:03:06 -0600

    Added content to the welcome screen. Stylized it in css. Added content to About.jsx. Added a google font to index.html

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -18,20 +18,33 @@ const About = () => {
       >
         <div className="about-content">
           <h2>Hi, I'm Landon Kimber!</h2>
+          <img
+            id="skiing-image"
+            src="./aboutImages/skiing.jpg"
+            alt="Picture of Landon skiing at Snow Basin"
+          />
           <p>
-            I am {birthAge} years old and was born and raised in Ogden, UT. I
-            enjoy all things in this beautiful state including hiking, fishing,
-            and skiing. While inside, I have always enjoyed using, learning, and
-            creating with technology. Whether it be choosing the correct parts
-            for my PC, creating digital artwork, playing video games, or coding,
-            I have always been fascinated with computers and the endless
-            possibilities computers bring. Coding in particaular has always been
-            an interest of mine because of the near limitless possibilities that
-            come with it. From following GameMaker tutorials on YouTube as a kid
-            to coding Java caclulators in high school, I have always enjoyed the
-            problem solving and creativity that comes with coding. While unsure
-            for the longest time after high school, I have now decided to pursue
-            a careet in web development. I have graduated from the University of
+            &emsp;I am {birthAge} years old and was born and raised in Ogden,
+            UT. I enjoy all things in this beautiful state including hiking,
+            fishing, and skiing. While inside, I have always enjoyed using,
+            learning, and creating with technology. Whether it be choosing the
+            correct parts for my PC, creating digital artwork, playing video
+            games, or coding, I have always been fascinated with computers and
+            the endless possibilities computers bring.
+          </p>
+          <img
+            id="rome-image"
+            src="./aboutImages/rome.jpg"
+            alt="Picture of Landon at the Roman Forum in Rome, Italy"
+          />
+          <p className="tab">
+            &emsp;Coding in particular has always been an interest of mine
+            because of the near limitless possibilities that come with it. From
+            following GameMaker tutorials on YouTube as a kid to coding Java
+            calculators in high school, I have always enjoyed the problem
+            solving and creativity that comes with coding. While unsure for the
+            longest time after high school, I have now decided to pursue a
+            career in web development. I have graduated from the University of
             Utah full stack development bootcamp and am currently looking to
             start my career in web development. Through the bootcamp, I have
             gained experience in HTML, CSS, JavaScript, Node.js, Express.js,
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const Home = ({ scrollToSection }) => {
   return (
-    <div className="welcome-section">
-      <h1>Welcome to My Portfolio</h1>
-      <p>Hi, I'm [Your Name]. I'm a [Your Profession/Title].</p>
-      <button onClick={() => scrollToSection("about")}>
-        Scroll to learn more
-      </button>
+    <div className="page-content">
+      <div className="welcome-container">
+        <h1 id="welcome" className="ubuntu">
+          Welcome!
+        </h1>
+        <h3 id="sub-welcome">Made by Landon Kimber</h3>
+        <button className="click-here" onClick={() => scrollToSection("about")}>
+          Click here!!
+        </button>
+      </div>
     </div>
   );
 };
